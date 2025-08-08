977684eceb8d6e3124542b87775cc5a56113ec14
Dias <dias.atudinov@gmail.com>
2025-08-08 19:44:06 +0500

    setup app

---
+++ S.Ana Star Gaming/S.Ana Star Gaming/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  S.Ana Star Gaming
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ S.Ana Star Gaming/S.Ana Star Gaming/S_Ana_Star_GamingApp.swift
@@ -0,0 +1,16 @@
+//
+//  S_Ana_Star_GamingApp.swift
+//  S.Ana Star Gaming
+//
+//
+
+import SwiftUI
+
+@main
+struct S_Ana_Star_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d6560eb5b164501e0e2e1a83699ab6505dff25b5
Dias <dias.atudinov@gmail.com>
2025-08-11 22:49:55 +0500

    add menu view and splash screen

--- S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGMenuView/ASGLoaderView.swift
+++ S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGMenuView/ASGLoaderView.swift
@@ -2,14 +2,68 @@
 //  ASGLoaderView.swift
 //  S.Ana Star Gaming
 //
-//  Created by Dias Atudinov on 10.08.2025.
 //
 
 import SwiftUI
 
 struct ASGLoaderView: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
+    
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ZStack {
+                Image(.loaderBgASG)
+                    .resizable()
+            }.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 50) {
+                Image(.appLogoASG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                VStack {
+                    Image(.loaderTextASG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 30)
+                    
+                    Image(.loaderImageASG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 80)
+                        .rotationEffect(.degrees(isAnimating ? 0 : 360))
+                        .animation(
+                            .linear(duration: 1) // скорость вращения
+                                .repeatForever(autoreverses: false),
+                            value: isAnimating
+                        )
+                        .onAppear {
+                            isAnimating = true
+                        }
+                }
+                Spacer()
+            }
+            .padding(.vertical, 50)
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
--- S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGMenuView/ASGMenuView.swift
+++ S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGMenuView/ASGMenuView.swift
@@ -2,15 +2,123 @@
 //  ASGMenuView.swift
 //  S.Ana Star Gaming
 //
-//  Created by Dias Atudinov on 10.08.2025.
 //
 
 import SwiftUI
 
 struct ASGMenuView: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyTask = false
+    
+//    @StateObject var achievementVM = NEGAchievementsViewModel()
+//    @StateObject var settingsVM = NGSettingsViewModel()
+//    @StateObject var shopVM = NEGShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            VStack(spacing: 0) {
+                HStack(alignment: .center, spacing: 20) {
+                    
+                    Spacer()
+                    
+                    NEGCoinBg()
+                    
+                    
+                    
+                }.padding(15)
+                
+                Spacer()
+                Image(.appLogoASG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                Spacer()
+                VStack(spacing: 20) {
+                    HStack(alignment: .top) {
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconASG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 120:60)
+                        }
+                    }
+                    
+                    Button {
+                        showShop = true
+                    } label: {
+                        Image(.shopIconASG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 120:60)
+                    }
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        Image(.achievementsIconASG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 120:60)
+                    }
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconASG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 120:60)
+                    }
+                }
+                Spacer()
+                
+                HStack {
+                    Button {
+                        showDailyTask = true
+                    } label: {
+                        Image(.dailyBonusIconASG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 140:80)
+                    }
+                    Spacer()
+                }
+            }.padding()
+        }
+        .background(
+            ZStack {
+                Image(.appBgASG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        .fullScreenCover(isPresented: $showGame) {
+//            NGRoundSelectionView()
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+//            NEGAchievementsView(viewModel: achievementVM)
+        }
+        .fullScreenCover(isPresented: $showShop) {
+//            NEGShopView(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+//            NEGSettingsView(settingsVM: settingsVM)
+        }
+        .fullScreenCover(isPresented: $showDailyTask) {
+//            DailyRewardsView()
+        }
     }
+    
 }
 
 #Preview {

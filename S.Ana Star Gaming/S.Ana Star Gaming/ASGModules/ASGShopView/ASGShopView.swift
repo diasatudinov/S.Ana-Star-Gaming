7bfeddfde3902f49571b3617e3c6a67c145c5950
Dias <dias.atudinov@gmail.com>
2025-08-12 19:12:22 +0500

    add shop view

--- S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGShopView/ASGShopView.swift
+++ S.Ana Star Gaming/S.Ana Star Gaming/ASGModules/ASGShopView/ASGShopView.swift
@@ -2,14 +2,83 @@
 //  ASGShopView.swift
 //  S.Ana Star Gaming
 //
-//  Created by Dias Atudinov on 12.08.2025.
 //
 
 import SwiftUI
 
 struct ASGShopView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject private var viewModel = NEGShopViewModel()
+    
+    private let columns = Array(repeating: GridItem(.flexible(), spacing: 12), count: 2)
+    private let dayCellHeight: CGFloat = NEGDeviceManager.shared.deviceType == .pad ? 350:200
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 0) {
+                
+                HStack(alignment: .top) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        Image(.backIconASG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 100:50)
+                    }
+                    Spacer()
+                    
+                    NEGCoinBg()
+                }.padding([.horizontal, .top])
+                
+                
+                
+                Image(.shopTextASG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 40:20)
+                Spacer()
+                ZStack {
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(viewModel.shopBgItems, id: \.self) { item in
+                            ZStack {
+                                Image(item.icon)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                
+                                VStack {
+                                    Spacer()
+                                    Image(.buyBtnASG)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 40:20)
+                                        .onTapGesture {
+                                            NEGUser.shared.minusUserMoney(for: item.price)
+                                        }
+                                }.padding(22)
+                                    
+                            }
+                            .frame(height: dayCellHeight)
+                        }
+                        
+                    }.frame(width: UIScreen.main.bounds.width - 150)
+                    
+                }
+                .frame(height: NEGDeviceManager.shared.deviceType == .pad ? 400:270)
+                Spacer()
+            }
+            .padding()
+            
+        }.background(
+            ZStack {
+                Image(.appBgASG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+            
+        )
     }
 }
 
